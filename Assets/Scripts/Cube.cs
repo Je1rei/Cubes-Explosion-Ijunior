39bfaf8e5997ddb906836055d4be4445fcf138e6
Je1rei <106481018+Je1rei@users.noreply.github.com>
2024-06-04 19:29:18 +0300

    Changed Raycast tracking

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,34 +2,22 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(Renderer), typeof(ClickableObject))]
+[RequireComponent(typeof(Rigidbody), typeof(Renderer))]
 public class Cube : MonoBehaviour
 {
     [SerializeField] private int _chanceSeparate = 100;
 
     private Rigidbody _rigidbody;
     private Renderer _renderer;
-    private ClickableObject _clickableObject;
 
     public int ChanceSeparate => _chanceSeparate;
 
     public event Action<Cube> Clicked;
 
-    private void OnEnable()
-    {
-        _clickableObject.Clicked += DestroyCube;
-    }
-
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _renderer = GetComponent<Renderer>();
-        _clickableObject = GetComponent<ClickableObject>();
-    }
-
-    private void OnDisable()
-    {
-        _clickableObject.Clicked -= DestroyCube;
     }
 
     public void DivideChance(int divider) => _chanceSeparate /= divider;
@@ -46,16 +34,13 @@ public class Cube : MonoBehaviour
         Vector3 direction = new Vector3(force * RandomValue(minValue, maxValue + 1),
                 force * RandomValue(minValue, maxValue + 1), force * RandomValue(minValue, maxValue + 1));
 
-        if (_rigidbody != null)
-        {
-            _rigidbody.AddForce(direction);
-        }
+        _rigidbody.AddForce(direction);
     }
 
-    private void DestroyCube()
+    public void DestroyCube()
     {
         Clicked?.Invoke(this);
-        Destroy(gameObject); 
+        Destroy(gameObject);
     }
 
     private float RandomValue(float minValue, float maxValue) => UnityEngine.Random.Range(minValue, maxValue);
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    private Ray _ray;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            ProcessClick();
+    }
+
+    public void ProcessClick()
+    {
+        _ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(_ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject.TryGetComponent<Cube>(out Cube cube))
+            {
+                cube.DestroyCube();
+            }
+        }
+    }
+}
+
+//1) ClickableObject - сколько кубов на сцене существует, столько рейкастов и будет обработано по нажатию мыши, 
+//    смысла в этом нет, это лишния работа для процессора, вам достаточно одного рейкаста при нажатии, 
+//    сравнение transform каждого куба с transform объекта на который нажали, не имеет никакого смысла, 
+//    ведь нужный объект найдётся только один, 
+//    нужно чтобы рейкасты обрабатывал отдельный объект не относящийся к кубам, 
+//    пусть он пускает рейкасты, ищет куб по лучу вызывает у него метод DestroyCube
