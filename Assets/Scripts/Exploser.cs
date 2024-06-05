7359b91227cd2489f7bb5f6e50518a87243ae62a
Je1rei <106481018+Je1rei@users.noreply.github.com>
2024-06-05 15:18:57 +0300

    Added power and radius scaleing

--- Assets/Scripts/Exploser.cs
+++ Assets/Scripts/Exploser.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    [SerializeField] private float _radiusOverlap = 1f;
+    [SerializeField] private float _baseRadiusOverlap = 1f;
     [SerializeField] private float _power = 5f;
     [SerializeField] private Generator _generator;
 
     private void OnEnable()
     {
-        _generator.Separated += Explode;    
+        _generator.Separated += Explode;
         _generator.Destroyed += Explode;
     }
 
@@ -29,14 +29,18 @@ public class Explosion : MonoBehaviour
 
     private void Explode(Cube currentCube)
     {
-        Collider[] colliders = Physics.OverlapSphere(currentCube.transform.position, _radiusOverlap);
+        float scale = currentCube.transform.position.x;
+        float power = _power / scale;
+        float radius = _baseRadiusOverlap / scale;
+
+        Collider[] colliders = Physics.OverlapSphere(currentCube.transform.position, radius);
 
         foreach (var collider in colliders)
         {
             if (collider.gameObject.TryGetComponent(out Cube cube))
             {
                 float distance = Vector3.Distance(currentCube.transform.position, cube.transform.position);
-                cube.AddForce(_power, distance);
+                cube.AddForce(power, distance);
             }
         }
     }
